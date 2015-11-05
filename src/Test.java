7fe4c65e092892af072b6de2023148e2d38a2cfd
Webster56 <w3b5t3r56@gmail.com>
2015-11-05 22:49:18 +0100

    added new functions to test file

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HTTPtransmitter transmitter = new HTTPtransmitter();
-		transmitter.requestUUID("Webster56");
+		//String uuid = transmitter.requestUUID("Rod_Vadar");
+		String uuid = transmitter.requestUUID("Webster");
+		transmitter.requestNameHistory(uuid);
+		transmitter.requestSkin(uuid);
 	}
 
 }
